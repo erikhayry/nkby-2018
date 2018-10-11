9e69f09521be1beedcef3436bb16c7f676903b37
Erik Portin <erikportin@Eriks-MacBook-Pro-2.local>
2018-10-11 21:28:04 +0200

    remove

--- api/index.js
+++ api/index.js
@@ -52,7 +52,28 @@ const approve = async (req, res) => {
     send(res, 200, editedLocations)
 };
 
+const disapprove = async (req, res) => {
+    const newEditedLocation = await json(req);
+    let editedLocations = JSON.parse(await read('data/edited-locations.json'));
+
+    if(editedLocations[newEditedLocation.id]){
+        let index = editedLocations[newEditedLocation.id].disapproved.findIndexOf((url) => {
+            return url === newEditedLocation.url        
+        })
+
+        if(index){
+            editedLocations[newEditedLocation.id].disapproved.splice(index, 0);
+        }
+    
+    }
+
+    await write('data/edited-locations.json', editedLocations);
+    editedLocations = await read('data/edited-locations.json');
+    send(res, 200, editedLocations)
+};
+
 module.exports = router(
     cors(post('/approve', approve)),
+    cors(post('/disapprove', disapprove)),
     cors(get('/get', getData))
 );
