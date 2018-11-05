c722394a28394280597d90520f45140176ad8965
Erik Portin <erik.portin@svt.se>
2018-11-05 09:21:09 +0100

    show starred

--- admin/pages/starred.js
+++ admin/pages/starred.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import 'isomorphic-unfetch'
 import { get } from '../utils/api'
+import locales from '../static/crawler-result-with-locales.json';
 
 export default class Index extends React.Component {
     static async getInitialProps () {
@@ -12,9 +13,55 @@ export default class Index extends React.Component {
     }
 
     render () {
-        console.log(this.props);
+        const {starredPages, editedLocales} = this.props;
+        const res = [];
+
+        starredPages.forEach(starredPage => {
+            let s = {
+                url: starredPage,
+                locales: []
+            };
+
+            Object.keys(editedLocales).forEach(key => {
+                const {approvedPages = []} = editedLocales[key];
+
+                const pageIndex = approvedPages.findIndex(approvedPage => {
+                    return approvedPage.url ===  starredPage;
+                })
+
+                if(pageIndex > -1){
+                    s.locales.push({id: key, ...approvedPages[pageIndex]})
+                }
+
+            })
+            
+            res.push(s)
+        })
+
         return (
             <div>
+                <h1>Starred</h1>
+                <ul>
+
+                    {res.map((starredPage, i) => {
+                        return (
+                            <li key={starredPage.url}>
+                                <h2>{starredPage.url}</h2>
+                                <ul>
+                                    {starredPage.locales.map((locale, i) => {
+                                        return (
+                                            <li key={i}>
+                                                {locale.id}
+                                            </li>
+                                        )
+                                    })}
+
+                                </ul>
+                            </li>
+                        )
+                    })}
+                </ul>
+
                 <Link prefetch href='/'><a>Back</a></Link>
             </div>
         )
