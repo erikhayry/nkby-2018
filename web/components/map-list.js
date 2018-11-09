db18e3663cea387eceec7e41a8a8ab2a0b86b072
Erik Portin <erik.portin@svt.se>
2018-11-09 22:05:08 +0100

    css map

--- web/components/map-list.js
+++ web/components/map-list.js
@@ -1,87 +1,112 @@
+import { useState } from 'react';
 import { sortPagesByTitle, sortLocalesByName, getMinMax } from '../utils'
-
-const lat = 63.5217687; // = 50%
-const lng = 22.5216011; // 50%
 
 let {min: latMin, max: latMax} = getMinMax('lat');
 let {min: lngMin, max: lngMax} = getMinMax('lng');
-//lngMax = 22.5510306;
-//latMax = 63.5319811;
-//
-const latWidth = (latMax - latMin);
-const lngWidth = (lngMax - lngMin);
-
-
-console.log(latMin, latMax);
-console.log(lngMin, lngMax);
-console.log(latWidth, lngWidth);
-console.log('mitt ', (latMin + latMax)/2, (lngMin + lngMax)/2);
-const midLng = (lngMax + lngMin) / 2;
-const lngC = midLng/22.5428763;
-
-const midLat = (latMax + latMin) / 2;
-const latC = 1 //midLat/63.5580228;
-//(lat*(mid/lat) - min) / (max - min)
 
 const MapList = ({locales}) => {
+    const [current, setCurrent] = useState('');
+    const [zoom, setZoom] = useState(0);
+    const z = zoom / 100;
+
+    let latMinZoomed = latMin + z;
+    let latMaxZoomed = latMax - z;
+
+    let lngMinZoomed = lngMin + z;
+    let lngMaxZoomed = lngMax - z;
+
+    let lngC = 1;
+    let latC = 1;
+
+    const latWidth = (latMaxZoomed - latMinZoomed);
+    const lngWidth = (lngMaxZoomed - lngMinZoomed);
+
+    if(current){
+        let centerLng = current.position.lng;
+        let centerLat = current.position.lat;
+
+        const midLng = (lngMaxZoomed + lngMinZoomed) / 2;
+        lngC = midLng/centerLng;
+
+        const midLat = (latMaxZoomed + latMinZoomed) / 2;
+        latC = midLat/centerLat;
+    }
+
     return (
-        <div style={{
-            position: 'relative',
-            width: '100vw',
-            height: '100vh',
-            backgroundColor: '#ccc'
-        }}>
-            <ul style={{
-                margin: 0,
-                listStyle: 'none' +
-                ''
+        <>
+            <style jsx global>{`
+                     :root {
+                        --lngC: ${lngC};
+                        --latC: ${latC};
+                     }
+
+                    `}</style>
+            <div style={{
+                position: 'relative',
+                width: `${400 * (lngWidth / latWidth)}px`,
+                height: `400px`,
+                backgroundColor: '#ccc',
+                margin: '0 auto'
             }}>
-                {sortLocalesByName(locales).map(({id, name, pages, position}, i) => {
-                    if(position){
-                        const zoom = 10;
-                        let lng = ((position.lng*lngC)-lngMin) / (lngWidth) * 100;
-                        //if(lng > 50){
-                        //    lng = lng + zoom
-                        //} else if(lng < 50){
-                        //    lng = lng - zoom
-                        //}
 
-                        let lat = ((position.lat*latC)-latMin) / (latWidth) * 100;
-                        //if(lat > 50){
-                        //    lat = lat + zoom
-                        //} else if(lat < 50){
-                        //    lat = lat - zoom
-                        //}
+                <div style={{
+                    position: 'absolute',
+                    top: 0,
+                    right: 0,
+                    zIndex: 1
+                }}>
+                    <button onClick={() => {
+                        setZoom((zoom === 9 ? zoom : zoom + 1))
+                    }}>+</button>
+                    <span>{`zoom ${zoom}`}</span>
+                    <button onClick={() => {
+                        setZoom(zoom === -9 ? zoom : zoom - 1)
+                    }}>-</button>
+                </div>
+                <ul style={{
+                    margin: 0,
+                    listStyle: 'none'
+                }}>
+                    {sortLocalesByName(locales).map(({id, name, pages, position}, i) => {
+                        if(position){
+                            let lng = ((position.lng*lngC)-lngMinZoomed) / (lngWidth) * 100;
+                            let lat = ((position.lat*latC)-latMinZoomed) / (latWidth) * 100;
 
-                        return (
-                            <li key={i} data-lng={position.lng} data-lat={position.lat} style={{
-                                position: 'absolute',
-                                left: `${lng}%`,
-                                bottom: `${lat}%`,
-                                transform: `translateX(${lng > 50 ? -100 : 0}%) translateY(${lat < 50 ? -100 : 0}%)`
-                            }}>
-                                <span id={id} style={{
-                                    //display: 'none'
-                                }}>{name}</span>
-                                <ul style={{
-                                    display: 'none'
+                            return (
+                                <li key={i} data-lng={position.lng} data-lat={position.lat} style={{
+                                    cursor: 'pointer',
+                                    position: 'absolute',
+                                    left: `calc(((${position.lng} * var(--lngC)) - ${lngMinZoomed}) / ${lngWidth} * 100%)`,
+                                    bottom: `calc(((${position.lat} * var(--latC)) - ${latMinZoomed}) / ${latWidth} * 100%)`,
+                                    transform: `translateX(${lng > 50 ? -100 : 0}%) translateY(${lat < 50 ? -100 : 0}%)`,
+                                    transition: 'left 0.3s ease-out, bottom 0.3s ease-out, transform 0.3s ease-out'
+
                                 }}>
-                                    {sortPagesByTitle(pages).map((page, i) => {
-                                        return (
-                                            <li key={i}>
-                                                <a href={page.url} target="_blank">{page.title}</a>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            </li>
-                        )
-                    }
+                                    <span id={id} style={{
+                                        fontSize: 8
+                                    }} onClick={() => {
+                                        setCurrent({id, position})
+                                    }}>{name}</span>
+                                    <ul style={{
+                                        display: 'none'
+                                    }}>
+                                        {sortPagesByTitle(pages).map((page, i) => {
+                                            return (
+                                                <li key={i}>
+                                                    <a href={page.url} target="_blank">{page.title}</a>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </li>
+                            )
+                        }
 
-                    return null;
-                })}
-            </ul>
-        </div>
+                        return null;
+                    })}
+                </ul>
+            </div>
+        </>
     )
 };
 
